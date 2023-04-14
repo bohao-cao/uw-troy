5e4222bb4ff6daeb5d0b52a74ce8f59a1bf9a656
Bohao Cao <bohao.cao@gmail.com>
2023-04-14 14:38:04 +0000

    optimized code

--- optimized.py
+++ optimized.py
@@ -1,98 +1,110 @@
+import fire
 import discord
 import os
-from llama_index import GPTSimpleVectorIndex, SimpleDirectoryReader, download_loader, ComposableGraph, GPTListIndex, GPTFaissIndex
+from llama_index import GPTSimpleVectorIndex, SimpleDirectoryReader, download_loader, ComposableGraph, GPTListIndex, GPTTreeIndex, LLMPredictor, ServiceContext
 from dotenv import load_dotenv
-import faiss
+from llama_index.evaluation import ResponseEvaluator
+from langchain.llms import OpenAI
 
-def load_faiss_index(index_name="faiss_index.json", path="./index/faiss_index.index"):
-    #path = os.path.join(path, index_name)
-    return GPTFaissIndex.load_from_disk(
-        index_name, 
-        faiss_index_save_path=path
-    )
+def index_io(action="save", path="./index/online_doc", index=None):
+    if action=="save":
+        index.save_to_disk(path)
+    else:
+        return GPTTreeIndex.load_from_disk(path)
+        
+
+def create_online_doc_index(test=True):
+    if test:
+        # just load one file for testing
+        online_documentation = SimpleDirectoryReader(input_files=['./data/announcements.rtf'], exclude_hidden=True).load_data()
+    else:
+        online_documentation = SimpleDirectoryReader('./data', recursive=True, exclude_hidden=True).load_data()
+    return GPTTreeIndex.from_documents(online_documentation)
+        
+
+def create_faq_index():
+    JSONReader = download_loader("JSONReader")
+    json_loader = JSONReader()
+    faq_docs = json_loader.load_data('./data/help-questions.json')
     
+    return GPTTreeIndex.from_documents(faq_docs)
+
+
+def init_reponse_evaluator():
+    llm_predictor_gpt3 = LLMPredictor(llm=OpenAI(temperature=0, model_name="text-davinci-003"))
+    service_context_gpt3 = ServiceContext.from_defaults(llm_predictor=llm_predictor_gpt3)
+
+    evaluator = ResponseEvaluator(service_context=service_context_gpt3)
+    return evaluator
+
+def start(test=True, create_index=False, evaluate_response=True):   
+    load_dotenv()
+
+    # channel ID for testing
+    # replace with you own channel ID
+    discord_channel = 1095448067802673244
+
+    if create_index:
+        online_doc_index = create_online_doc_index()
+        faq_index = create_faq_index()
+        index_io("save", path="./index/online_doc", index=online_doc_index)
+        index_io("save", path="./index/faq", index=faq_index)
+    else:
+        online_doc_index = index_io("load", path="./index/online_doc")
+        faq_index = index_io("load", path="./index/faq")
+
+
+    evaluator = init_reponse_evaluator() if evaluate_response else None
+
+    graph = ComposableGraph.from_indices(GPTListIndex, [online_doc_index, faq_index], index_summaries=["OO Docs", "Help Questions"])
+
+    intents = discord.Intents().all()
+    intents.message_content = True
     
-def save_faiss_index(index, index_name="faiss_index.json", path="./index/faiss_index.index"):
-    #path = os.path.join(path, index_name)
-    index.save_to_disk(
-        index_name,
-        faiss_index_save_path=path
-    )
+    client = discord.Client(intents=intents)
     
+    @client.event
+    async def on_ready():
+        print(f'Logged in as user {client.user}')
+
+    @client.event    
+    async def on_thread_create(thread):
+        print(f"thread:{thread}")
+        message = await thread.fetch_message(thread.id)
+
+        if thread.parent_id != discord_channel:
+            return
+
+        response = graph.query(message.content if message.content else thread.name)
+        
+        if evaluator:
+            eval_result = evaluator.evaluate(response)
+            if "YES" in eval_result:
+                print("response is evaluated and positive")
+            else:
+                print("response is evaluated and negative, use with caution!")
+
+        await thread.send(response)
+
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+        message_content = message.content
+        print(f"Received a question: {message_content}")
+        response = graph.query(message_content)
+
+        if evaluator:
+            eval_result = evaluator.evaluate(response)
+            if "YES" in eval_result:
+                print("response is evaluated and positive")
+            else:
+                print("response is evaluated and negative, use with caution!")
+        await message.reply(response, mention_author=True)
     
-def create_faiss_index(documents):
-    # dimensions of text-ada-embedding-002
-    d = 1536 
-    faiss_index = faiss.IndexFlatL2(d)
-    index = GPTFaissIndex.from_documents(documents, faiss_index=faiss_index)
-    
-    return index
-
-load_dotenv()
-
-intents = discord.Intents().all()
-
-discord_token = os.getenv('DISCORD_TOKEN')
-# channel ID for testing
-# replace with you own channel ID
-discord_channel = 1095448067802673244
+    discord_token = os.getenv('DISCORD_TOKEN')
+    client.run(discord_token)
 
 
-#online_documentation = SimpleDirectoryReader('./data', recursive=True, exclude_hidden=True).load_data()
-online_documentation = SimpleDirectoryReader(input_files=['./data/announcements.rtf'], exclude_hidden=True).load_data()
-online_doc_index = create_faiss_index(online_documentation)
-save_faiss_index(online_doc_index, "./index/online_doc_index.json", "./index/online_doc_index.index")
-
-
-JSONReader = download_loader("JSONReader")
-json_loader = JSONReader()
-common_help_questions = json_loader.load_data('./data/help-questions.json')
-common_help_questions_index = create_faiss_index(online_documentation)
-save_faiss_index(common_help_questions_index, "./index/common_help_questions_index.json", "./index/common_help_questions_index.index")
-
-
-# index_online_docs = GPTSimpleVectorIndex.from_documents(online_documentation)
-# index_good_questions = GPTSimpleVectorIndex.from_documents(common_help_questions)
-
-graph = ComposableGraph.from_indices(GPTListIndex, [common_help_questions_index, online_doc_index], index_summaries=["OO Docs", "Help Questions"])
-
-client = discord.Client(intents=intents)
-
-@client.event
-async def on_ready():
-    print(f'We have logged in as {client.user}')
-
-
-@client.event
-async def on_thread_create(thread):
-    print(f"thread:{thread}")
-    message = await thread.fetch_message(thread.id)
-
-    if thread.parent_id != discord_channel:
-        return
-
-    response = graph.query(message.content if message.content else thread.name)
-    await thread.send(response)
-
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    message_content = message.content
-    print(f"Received a question: {message_content}")
-    response = graph.query(message_content)
-    
-
-    await message.channel.send(response) 
-    
-client.run(discord_token)
-
-
-
-    
-    
-
-
-
-
+if __name__ == "__main__":
+    fire.Fire(start)
